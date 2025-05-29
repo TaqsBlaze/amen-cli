165a5b1c6c8994daae19111f3663565fd2043e36
TaqsBlaze <tanakah30@gmail.com>
2025-05-29 12:44:20 +0200

    updated docs

--- amen/__init__.py
+++ amen/__init__.py
@@ -1,5 +1,5 @@
 """AMEN CLI - Python Web Framework Scaffolding Tool"""
-__version__ = "0.4.5"
+__version__ = "0.4.6"
 __author__ = "Tanaka Chinengundu"
 
 __description__ = "Inspired by the laravel installer, Python Web Application Scaffolding Tool"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="amen-cli",
-    version="0.4.5",
+    version="0.4.6",
     author="Tanaka Chinengundu",
     author_email="tanakah30@gmail.com",
     description="composer-inspired Python web framework scaffolding tool",
