742655b32505e4a7c7774a31576f7aa1dd2b7baf
TaqsBlaze <tanakah30@gmail.com>
2025-05-30 12:47:08 +0200

    v0.5.0

--- amen/__init__.py
+++ amen/__init__.py
@@ -1,5 +1,5 @@
 """AMEN CLI - Python Web Application Scaffolding Tool"""
-__version__ = "0.4.7"
+__version__ = "0.5.0"
 __author__ = "Tanaka Chinengundu"
 
 __description__ = "Inspired by the laravel installer, Python Web Application Scaffolding Tool"
