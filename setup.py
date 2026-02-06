d10c1264def87201025f1741ddc1e35ef6d24064
TaqsBlaze <tanakah30@gmail.com>
2026-02-06 21:41:04 +0200

    version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="amen-cli",
-    version="0.9.1",
+    version="0.10.1",
     author="Tanaka Chinengundu",
     author_email="tanakah30@gmail.com",
     description="composer-inspired Python web framework scaffolding tool",
@@ -27,7 +27,8 @@ setup(
         "rich>=0.1.0",
         "colorama>=0.4.4",
         "tabulate>=0.8.9",
-        "questionary>=1.10.0"
+        "questionary>=1.10.0",
+        "pytz"
     ],
     entry_points={
         'console_scripts': [
